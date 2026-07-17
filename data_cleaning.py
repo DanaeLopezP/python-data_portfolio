aa091eb49af179a0b968a57a622d4f1a3e351831
A01745689pd <114614697+DanaeLopezP@users.noreply.github.com>
2026-07-16 18:23:40 -0600

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -6,6 +6,19 @@ import pandas as pd # pd is an alias for our library
 # DATA LOAD
 # Pandas allows you to read different types of archives
 
-df1 = pd.read_excel('data.xls')
+#df1 = pd.read_excel('data.xls')
 
 df2 = pd.read_csv('dataCGP.csv')
+
+#DATA INSPECTION
+#print(df2.dtypes)
+#print(df2.describe())
+#print(df2.head())
+#print(df2.tail())
+#df2.info()
+
+#DATA CLEANING
+#print(df2.isnull().sum())
+df2.rename(columns={'Precio Unitario': 'PRECIO'}, inplace = True)
+df2 = df2.drop(columns=['Cantidad'])
+print(df2.head())
