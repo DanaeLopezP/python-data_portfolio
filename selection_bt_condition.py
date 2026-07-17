f2b88de4a7bb06b78c7d0414945904f8af931fed
A01745689pd <114614697+DanaeLopezP@users.noreply.github.com>
2026-07-17 14:41:52 -0600

    Selection by condition problems pt 2

--- selection_bt_condition.py
+++ selection_bt_condition.py
@@ -15,20 +15,55 @@ df = pd.DataFrame(datos)
 #ONE CONDITION
 
 #Only +18
-#df1 = df[df['Edad'] > 17]
+df1 = df[df['Edad'] > 17]
 #print(df1)
 #Only younger than 25
-#df2 = df[df['Edad'] < 25 ]
+df2 = df[df['Edad'] < 25 ]
 #print(df2)
 #Only people who lives in CDMX
-#df3 = df[df['Ciudad']== 'CDMX']
+df3 = df[df['Ciudad']== 'CDMX']
 #print(df3)
 #Only salary upper than 20,000
-#df4 = df[df['Salario'] > 20000]
+df4 = df[df['Salario'] > 20000]
 #print(df4)
 #Only Marias
-#df5 = df[df['Nombre'] == 'María']
+df5 = df[df['Nombre'] == 'María']
+print(df.loc[df['Nombre'] == 'María']) #Useful way
 
-print(df.loc[df['Nombre'] == 'María'])
+#TWO CONDITIONS W AND
 
+#Only +18 and Monterrey
+print(df.loc[(df['Edad'] > 18) & (df['Ciudad']== 'Monterrey')])
+#Only +30 and Salary upper 30000
+print(df.loc[(df['Edad'] > 30) & (df['Salario'] > 30000 )])
+#Only -18 and Puebla
+print(df.loc[(df['Edad'] < 18) & (df['Ciudad'] == 'Puebla' )])
+#Only Guadalajara and salari lower than 30000
+print(df.loc[(df['Ciudad'] == 'Guadalajara') & (df['Salario'] < 30000 )])
 
+#TWO CONDITIONS W OR
+
+#City CDMX or Guadalajara
+print(df.loc[(df['Ciudad'] == 'CDMX') | (df["Ciudad"] == 'Guadalajara')])
+#Only -18 or salary lower than 10000
+print(df.loc[(df['Edad'] < 18) | (df["Salario"] < 10000)])
+#Name is Ana or Maria
+print(df.loc[(df['Nombre'] == 'Ana') | (df["Nombre"] == 'María')])
+
+#NEGATION
+
+#No living in Monterrey
+print(df.loc[~(df["Ciudad"] == "CDMX")])
+#No +18 and no Monterrey
+print(df.loc[~(df["Edad"] > 17) & ~(df["Ciudad"] == 'Monterrey')])
+#No salary upper than 20000
+print(df.loc[~(df["Salario"] > 20000)])
+
+#MULTIPLE SELECTION COLUMN W CONDITION
+print(df.loc[df['Salario'] > 20000, ['Nombre', 'Salario']])
+
+#MULTIPLE CONDITIONS
+print(df.loc[(df['Edad'] > 18) & (df['Ciudad'] == 'CDMX') | (df['Ciudad'] == 'Puebla') & (df['Salario'] > 15000)])
+
+#CHALLENGE
+print(df.loc[(df['Edad'] >= 25) & (df['Salario'] < 30000) & ~(df["Ciudad"] == "Monterrey"), ['Nombre', 'Ciudad', 'Salario']])
